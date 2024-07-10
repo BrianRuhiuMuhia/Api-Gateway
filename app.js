4040bd6b8dc33c4970e23d168c8948d562180ce0
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2024-07-10 14:07:00 +0300

    third_commit

--- app.js
+++ app.js
@@ -11,7 +11,18 @@ app.use(helmet());
 app.use(morgan("combined")); 
 app.use(limiter);
 app.disable("x-powered-by");
-const PORT = process.env.PORT || 5000;
+services.forEach(({ route, target }) => {
+    const proxy = {
+      target,
+      changeOrigin: true,
+      pathRewrite: {
+        [`^${route}`]: "",
+      },
+    };
+  
+    app.use(route,createProxyMiddleware(proxy));
+  });
+const PORT = process.env.GATEWAY_PORT || 3000;
 app.listen(PORT, () => {
  console.log(`Gateway is running on port ${PORT}`);
 });
