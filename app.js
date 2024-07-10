4b3d711d53099c106806776356a829c1e9ab1bdc
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2024-07-10 13:11:13 +0300

    first commit

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const services=require("./services.js")
+const services=require("./services/services.js")
+const limiter=require("./rateLimiter/rateLimiter.js")
 const app = express();
 app.use(cors()); 
 app.use(helmet()); 
 app.use(morgan("combined")); 
+app.use(limiter);
 app.disable("x-powered-by");
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
